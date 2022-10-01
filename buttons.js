941135ebb9e16e3b457469c4199f528fcfb6ef5f
Mateusz Kluska <mateuszkluska7@gmail.com>
2022-10-01 15:13:46 +0200

    add some functionality

---
+++ buttons.js
@@ -0,0 +1,16 @@
+// const taskText = document.querySelectorAll(".task-text")
+// const check = `<svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>`
+
+// const addClassToSpan = () => {
+//     taskText.forEach(span => span.classList.add("text-check"))
+// }
+
+//     buttons.forEach(button => button.addEventListener("click", function(){
+//         button.innerHTML = check
+//         button.classList.add("btn-check")
+//         addClassToSpan()
+        
+//     }))
+ 
+
+
--- taskCreator.js
+++ taskCreator.js
@@ -2,12 +2,15 @@ const input = document.getElementById("addInputBar")
 const btn = document.querySelector(".add-input")
 const toDoList = document.querySelector(".input-list")
 console.log(input)
+let buttons = document.querySelectorAll(".btn-nocheck")
 
 const createElements = () => {
     let createButton = document.createElement("button")
     let createSpan = document.createElement("span")
     createButton.classList.add("btn-nocheck")
+    createSpan.classList.add("task-text")
     createSpan.innerText = input.value
+    createDiv.classList.add("task-container")
     createDiv.appendChild(createButton)
     createDiv.appendChild(createSpan)
 }
@@ -18,7 +21,7 @@ const createDivTest = () => {
         toDoList.appendChild(createDiv)
         createElements()
         input.value = ""
+        return buttons = document.querySelectorAll(".btn-nocheck")
     })
 }
-
 createDivTest()
