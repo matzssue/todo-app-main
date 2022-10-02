38b32994b81f83429c473dfae81fc0698e8b8070
Mateusz Kluska <mateuszkluska7@gmail.com>
2022-10-02 21:26:35 +0200

    add styles in js to buttons "check" and text next to it.

--- taskCreator.js
+++ taskCreator.js
@@ -1,8 +1,9 @@
+
 const input = document.getElementById("addInputBar")
 const btn = document.querySelector(".add-input")
 const toDoList = document.querySelector(".input-list")
-console.log(input)
-let buttons = document.querySelectorAll(".btn-nocheck")
+let createDiv = document.createElement("div")
+
 
 const createElements = () => {
     let createButton = document.createElement("button")
@@ -15,13 +16,34 @@ const createElements = () => {
     createDiv.appendChild(createSpan)
 }
 
+const check = `<svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>`
+
+
+
+
+
+function addStyles() {
+    buttons = document.querySelectorAll(".btn-nocheck")
+    taskText = document.querySelectorAll(".task-text")
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+        button.innerHTML = check
+        button.classList.add("btn-check")
+        button.nextElementSibling.classList.add("text-check")
+
+    })
+})
+}
 const createDivTest = () => {
     btn.addEventListener("click", function(){
         createDiv = document.createElement("div")
         toDoList.appendChild(createDiv)
         createElements()
         input.value = ""
-        return buttons = document.querySelectorAll(".btn-nocheck")
+        addStyles()  
+
     })
+    
 }
 createDivTest()
+
